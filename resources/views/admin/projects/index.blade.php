cbd0e69a223134653eac8dded79b77d0075a98bc
DavideGila <davide.gila@gmail.com>
2024-01-12 16:30:57 +0100

    added a button to create

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,5 +5,6 @@
         @foreach ($projects as $project)
             <p><a href="{{ route('admin.projects.show', $project->id) }}">{{ $project->title }}</a></p>
         @endforeach
+        <button class="btn btn-primary"><a href="{{route('admin.projects.create', $project)}}" class="text-white text-decoration-none"  >Crea</a></button>
     </section>
 @endsection
