2bc9ee62ef26621ff2dbc797ed1587a8e17d4123
DavideGila <davide.gila@gmail.com>
2024-01-12 16:31:09 +0100

    added a button to edit

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,5 +3,7 @@
     <section class="container">
         <h1>{{$project->title}}</h1>
         <p>{{$project->body}}</p>
+
+        <button class="btn btn-primary"><a href="{{route('admin.projects.edit', $project)}}" class="text-white text-decoration-none">Edit</a></button>
     </section>
 @endsection
